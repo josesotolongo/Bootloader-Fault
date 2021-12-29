2869f68f78686f294df8e77477436cfb2320e419
Jose Sotolongo <94718943+josesotolongo@users.noreply.github.com>
2021-12-29 12:48:52 -0500

    First successful build of the application

---
+++ nRF5SDK160098a08e2/examples/dfu/bootloader_fault/pca10040_s132_ble_debug/ses/Output/Release/Obj/secure_bootloader_ble_s132_pca10040_debug/oberon_backend_ecc.d
@@ -0,0 +1,4 @@
+Output/Release/Obj/secure_bootloader_ble_s132_pca10040_debug/oberon_backend_ecc.o: \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/components/libraries/crypto/backend/oberon/oberon_backend_ecc.c \
+ ../config/sdk_config.h \
+ ../../../../../components/libraries/util/nordic_common.h
---
+++ nRF5SDK160098a08e2/examples/dfu/bootloader_fault/pca10040_s132_ble_debug/ses/Output/Release/Obj/secure_bootloader_ble_s132_pca10040_debug/system_nrf52.d
@@ -0,0 +1,17 @@
+Output/Release/Obj/secure_bootloader_ble_s132_pca10040_debug/system_nrf52.o: \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/modules/nrfx/mdk/system_nrf52.c \
+ /usr/share/segger_embedded_studio_for_arm_5.68/include/stdint.h \
+ /usr/share/segger_embedded_studio_for_arm_5.68/include/stdbool.h \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/modules/nrfx/mdk/nrf.h \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/modules/nrfx/mdk/nrf52.h \
+ ../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/modules/nrfx/mdk/system_nrf52.h \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/modules/nrfx/mdk/system_nrf.h \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/modules/nrfx/mdk/nrf52_bitfields.h \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/modules/nrfx/mdk/nrf51_to_nrf52.h \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/modules/nrfx/mdk/nrf52_name_change.h \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/modules/nrfx/mdk/compiler_abstraction.h
