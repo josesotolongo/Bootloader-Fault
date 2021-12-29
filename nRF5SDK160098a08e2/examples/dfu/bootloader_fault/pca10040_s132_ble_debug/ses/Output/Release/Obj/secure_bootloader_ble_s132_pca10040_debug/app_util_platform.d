aa5b326efecda3548ee2f5d9ffcfa0873a9de886
Jose Sotolongo <94718943+josesotolongo@users.noreply.github.com>
2021-12-29 12:47:28 -0500

    Initial bootloader_fault build and added micro-ecc to external

---
+++ nRF5SDK160098a08e2/examples/dfu/bootloader_fault/pca10040_s132_ble_debug/ses/Output/Release/Obj/secure_bootloader_ble_s132_pca10040_debug/app_util_platform.d
@@ -0,0 +1,32 @@
+Output/Release/Obj/secure_bootloader_ble_s132_pca10040_debug/app_util_platform.o: \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/components/libraries/util/app_util_platform.c \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/components/libraries/util/app_util_platform.h \
+ /usr/share/segger_embedded_studio_for_arm_5.68/include/stdint.h \
+ ../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../modules/nrfx/mdk/nrf52.h \
+ ../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../modules/nrfx/mdk/system_nrf52.h \
+ ../../../../../modules/nrfx/mdk/system_nrf.h \
+ ../../../../../modules/nrfx/mdk/nrf52_bitfields.h \
+ ../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../../../../../modules/nrfx/mdk/nrf52_name_change.h \
+ ../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../components/softdevice/s132/headers/nrf_soc.h \
+ ../../../../../components/softdevice/s132/headers/nrf_svc.h \
+ ../../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../components/softdevice/s132/headers/nrf_error_soc.h \
+ ../../../../../components/softdevice/s132/headers/nrf_nvic.h \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/components/libraries/util/nrf_assert.h \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/components/libraries/util/app_error.h \
+ /usr/share/segger_embedded_studio_for_arm_5.68/include/stdio.h \
+ /usr/share/segger_embedded_studio_for_arm_5.68/include/__crossworks.h \
+ /usr/share/segger_embedded_studio_for_arm_5.68/include/stdbool.h \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/components/libraries/util/sdk_errors.h \
+ ../../../../../components/softdevice/s132/headers/nrf_error.h \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/components/libraries/util/nordic_common.h \
+ /home/dex/Documents/GitHub/Bootloader-Fault/nRF5SDK160098a08e2/components/libraries/util/app_error_weak.h
